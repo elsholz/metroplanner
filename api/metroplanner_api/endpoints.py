b0620e810a9c243e61ada16f4abd54ab26fa361c
elsholz <hendrik.lankers.hl@googlemail.com>
2023-02-21 21:34:50 +0100

    add stats

--- api/metroplanner_api/endpoints.py
+++ api/metroplanner_api/endpoints.py
@@ -3,6 +3,7 @@ from typing import Dict
 from methods import GET, POST, PATCH, DELETE
 import responses
 import environment
+from bson.objectid import ObjectId
 
 
 class Endpoint(ABC):
@@ -43,15 +44,71 @@ class PublicEndpoint(Endpoint):
                 try:
                     links_collection = self.env.get_database().links
                     plans_collection = self.env.get_database().plans
+                    stats_collection = self.env.get_database().stats
 
                     link_result = links_collection.find_one({"_id": shortlink})
-                    print('Link result:', link_result)
+                    print("Link result:", link_result)
+                    if link_result:
+                        if link_result["active"]:
+                            plan_id = link_result["plan"]
+                            plan_result = plans_collection.find_one(
+                                {"_id": plan_id},
+                                {
+                                    "_id": 0,
+                                    "planName": 1,
+                                    "ownedBy": 1,
+                                    "forkedFrom": 1,
+                                    "deleted": 1,
+                                    "createdAt": 1,
+                                    "lastModifiedAt": 1,
+                                    "likeCount": 1,
+                                    "currentColorTheme": 1,
+                                    "currentNumberOfEdges": 1,
+                                    "currentNumberOfLabels": 1,
+                                    "currentNumberOfLines": 1,
+                                    "currentNumberOfNodes": 1,
+                                },
+                            )
+                            if plan_result:
+                                if plan_result["deleted"]:
+                                    print(
+                                        f"Error: Plan with ID {plan_id} for "
+                                        f"Shortlink {shortlink} has been deleted."
+                                    )
+                                    return responses.not_found_404()
+                                else:
+                                    stats_result = stats_collection.find_one(
+                                        {
+                                            "_id": {
+                                                "plan": plan_id,
+                                                "link": shortlink,
+                                            }
+                                        },
+                                        {
+                                            "totalCound": 1,
+                                            "_id": 0,
+                                        },
+                                    )
+                                    plan_result['totalViewCound'] = stats_result['totalCount']
+                                    for k,v in plan_result.items():
+                                        if isinstance(v, ObjectId):
+                                            k[v] = str(v)
+                                    return responses.ok_200(plan_result)
+                            else:
+                                print(
+                                    f"Error: Plan with ID {plan_id} for "
+                                    f"Shortlink {shortlink} does not exist."
+                                )
+                                return responses.not_found_404()
+                        else:
+                            return responses.not_found_404()
+                    else:
+                        return responses.not_found_404()
                     return responses.ok_200(link_result)
 
                 except Exception as e:
                     print("Exception!!:", e)
                     return responses.ok_200(str(e))
-
 
         children = {GET: GetPlan}
 
