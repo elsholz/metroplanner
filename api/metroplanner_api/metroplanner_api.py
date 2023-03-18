7ff14b8f220aade90b7d2d9c0c923e8bd0e686de
elsholz <hendrik.lankers.hl@googlemail.com>
2023-03-18 20:02:40 +0100

    fix imports

--- api/metroplanner_api/metroplanner_api.py
+++ api/metroplanner_api/metroplanner_api.py
@@ -1,7 +1,4 @@
 import json
-from jsonschema import validate, ValidationError
-import schemas
-import base64
 import endpoints
 import responses
 import environment
@@ -14,69 +11,73 @@ def private_handler(route, method, event, context, env: environment.Environment)
     try:
         sub = env.check_auth(event)
         try:
-            endpoint: endpoints.EndpointCollection = endpoints.PrivateEndpoint.children[route]
+            endpoint: endpoints.EndpointCollection = endpoints.PrivateEndpoint.children[
+                route
+            ]
             try:
                 endpoint_method: endpoints.EndpointMethod = endpoint.children[method]
                 try:
                     action = endpoint_method(event, context, env, sub)
                     return action()
                 except Exception as e:
-                    print('Exception calling endpoint method:', e)
+                    print("Exception calling endpoint method:", e)
                     return responses.internal_server_error_500()
             except KeyError as e:
                 return responses.method_not_allowed_405()
             except Exception as e:
-                print('Exception getting endpoint method:', e)
+                print("Exception getting endpoint method:", e)
                 return responses.internal_server_error_500()
         except KeyError as e:
-            print('Exception in private handler:', e)
+            print("Exception in private handler:", e)
             return responses.bad_request_400()
         except Exception as e:
-            print('Exception in private handler:', e)
+            print("Exception in private handler:", e)
             return responses.internal_server_error_500()
     except environment.BadRequestError as e:
-        print('Exception in private handler:', e)
+        print("Exception in private handler:", e)
         return responses.bad_request_400()
     except environment.InvalidTokenError as e:
-        print('Exception in private handler:', e)
+        print("Exception in private handler:", e)
         return responses.unauthorized_401()
     except Exception as e:
-        print('Exception in private handler:', e)
+        print("Exception in private handler:", e)
         return responses.internal_server_error_500()
-  
+
 
 def public_handler(route, method, event, context, env: environment.Environment):
     try:
-        endpoint: endpoints.EndpointCollection = endpoints.PublicEndpoint.children[route]
+        endpoint: endpoints.EndpointCollection = endpoints.PublicEndpoint.children[
+            route
+        ]
         try:
             endpoint_method: endpoints.EndpointMethod = endpoint.children[method]
             try:
                 action = endpoint_method(event, context, env)
                 return action()
             except Exception as e:
-                print('Exception calling endpoint method:', e)
+                print("Exception calling endpoint method:", e)
                 return responses.internal_server_error_500()
         except KeyError as e:
             return responses.method_not_allowed_405()
         except Exception as e:
-            print('Exception getting endpoint method:', e)
+            print("Exception getting endpoint method:", e)
             return responses.internal_server_error_500()
     except KeyError as e:
         return responses.bad_request_400()
     except Exception as e:
-        print('Exception in public handler:', e)
+        print("Exception in public handler:", e)
         return responses.internal_server_error_500()
 
 
 def lambda_handler(event, context):
     try:
         print("Event:", event)
-        print('Context:', context)
+        print("Context:", context)
 
         request_context = event["requestContext"]
 
         if not ENV.is_initialized:
-            print('Initializing Environment')
+            print("Initializing Environment")
             ENV.initialize_environment(request_context["stage"])
 
         route_key = event["routeKey"]
@@ -89,20 +90,20 @@ def lambda_handler(event, context):
         user_agent = http.get("userAgent", None)
         request_id = request_context["requestId"]
 
-        print('Method:', method)
-        print('RoutePath:', route_path)
+        print("Method:", method)
+        print("RoutePath:", route_path)
 
         if route_path.startswith("/api/_"):
-            print('Calling private handler')
+            print("Calling private handler")
             res = private_handler(route_path, method, event, context, ENV)
         else:
-            print('Calling public handler')
+            print("Calling public handler")
             res = public_handler(route_path, method, event, context, ENV)
     except Exception as e:
         print("Exception handling request:", e)
         res = responses.internal_server_error_500()
 
-    print('Result to be returned:', res)
+    print("Result to be returned:", res)
 
     try:
         ENV.send_log_message(
@@ -127,93 +128,3 @@ def lambda_handler(event, context):
         print(e, res)
 
     return res
-
-    try:
-        stage = event["requestContext"]["stage"]
-        assert stage in [globals.DEV, globals.PROD]
-        userdata_collection = database.get_userdata_collection(stage)
-    except Exception as e:
-        return {"statusCode": 400, "body": "Bad Request"}
-
-    try:
-        try:
-            userid = auth.check_auth(event)
-        except Exception as e:
-            print(e)
-            return {"statusCode": 401, "body": "Not Authorized"}
-
-        method = event["requestContext"]["http"]["method"]
-
-        if method == "GET":
-            try:
-                userdata = userdata_collection.find_one({"_id": userid})
-            except Exception as e:
-                print(e)
-                return {"statusCode": 500, "body": "Internal Server Error"}
-            try:
-                if not userdata:
-                    userdata = userdata or {"_id": userid, "hours": {}}
-                    userdata_collection.insert_one(userdata)
-
-                return {
-                    "statusCode": 200,
-                    "body": json.dumps(userdata, ensure_ascii=False, indent=4),
-                }
-            except Exception as e:
-                print(e)
-                return {"statusCode": 500, "body": "Internal Server Error"}
-        elif method == "PATCH":
-            try:
-                data = json.loads(base64.b64decode(event.get("body", None)))
-                try:
-                    validate(instance=data, schema=schemas.patch_request_schema)
-
-                    year, month, day = data["year"], data["month"], data["day"]
-                    hours = data["hours"]
-
-                    userdata_collection.update_one(
-                        {
-                            "_id": userid,
-                        },
-                        {
-                            "$set": {
-                                f"hours.{year}.{str(month).zfill(2)}.{str(day).zfill(2)}": hours
-                            }
-                        },
-                        upsert=True,
-                    )
-
-                    return {"statusCode": 200, "body": "OK"}
-                except ValidationError as e:
-                    print(e)
-                    return {"statusCode": 401, "body": ""}
-                except Exception as e:
-                    print(e)
-                    return {"statusCode": 500, "body": ""}
-            except Exception as e:
-                print(e)
-                return {"statusCode": 401, "body": ""}
-
-        elif method == "DELETE":
-            pass
-        else:
-            print("Invalid Method:", method)
-            return {
-                "statusCode": 501,
-                "body": json.dumps(
-                    {
-                        "message": "Method not implemented",
-                    }
-                ),
-            }
-
-    except Exception as e:
-        print(e)
-        return {
-            "statusCode": 500,
-            "body": json.dumps(
-                {
-                    "message": "Internal Server Error",
-                }
-            ),
-        }
