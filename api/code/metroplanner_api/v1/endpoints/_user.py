9f61e83c8dd05098c0519019a2f1e446ce1ef9f5
elsholz <hendrik.lankers.hl@googlemail.com>
2024-01-07 01:09:59 +0100

    minor change for user api

--- api/code/metroplanner_api/v1/endpoints/_user.py
+++ api/code/metroplanner_api/v1/endpoints/_user.py
@@ -14,65 +14,69 @@ router = APIRouter()
 def get_user(
     request: Request, sub: str = Depends(check_auth)
 ) -> type_definitions.UserInDB:
-        db = ENV.database
-        user_result = db.users.find_one({"_id": sub})
-        if user_result:
-            print("Found User Profile:", user_result)
+    db = ENV.database
+    user_result = db.users.find_one({"_id": sub})
+    if user_result:
+        print("Found User Profile:", user_result)
 
-            plans_created = list(
-                db.plans.find(
-                    {"ownedBy": sub, "deleted": None},
-                    {
-                        "planName": 1,
-                        "planDescription": 1,
-                        "_id": 1,
-                        "planShortlink": 1,
-                        "public": 1,
-                    },
-                )
+        plans_created = list(
+            p
+            for p in db.plans.find(
+                {"ownedBy": sub},
+                {
+                    "planName": 1,
+                    "planDescription": 1,
+                    "_id": 1,
+                    "planShortlink": 1,  # TODO: Primary Shorlink
+                    # "primaryShortlink": 1,
+                    "public": 1,
+                    "deleted": 1,
+                },
             )
-            for p in plans_created:
-                p["planId"] = str(p["_id"])
+            if not p.get("deleted", False)
+        )
 
-                if not "planShortlink" in p:
-                    plan_link = db.links.find_one({"plan": p["_id"]})
-                    if plan_link:
-                        p["planShortlink"] = plan_link["_id"]
+        for p in plans_created:
+            p["planId"] = str(p["_id"])
 
-                del p["_id"]
+            if not "planShortlink" in p:
+                plan_link = db.links.find_one({"plan": p["_id"]})
+                if plan_link:
+                    p["planShortlink"] = plan_link["_id"]
 
-            likes = []
-            for liked_planid in user_result["likesGiven"]:
-                print("Get shortlink for plan with id", liked_planid)
-                liked_plan_shortlink = db.links.find_one(
-                    {"plan": liked_planid}, {"_id": 1}
-                )
-                print("Liked plan Shortlink:", liked_plan_shortlink)
-                likes.append(liked_plan_shortlink["_id"])
-            user_result["likesGiven"] = likes
+            del p["_id"]
 
-            print("plans as list: ", plans_created)
-            user_result["plansCreated"] = plans_created
+        likes = []
 
-            return user_result
-        else:
-            print("User profile not found, creating profile.")
-            user_creation_result = db.users.insert_one(
-                user_data := {
-                    "_id": sub,
-                    "displayName": "",
-                    "public": False,
-                    "profileViews": 0,
-                    "likesGiven": [],
-                    "profilePicture": None,
-                    "bio": "",
-                }
-            )
+        for liked_planid in user_result["likesGiven"]:
+            print("Get shortlink for plan with id", liked_planid)
+            liked_plan_shortlink = db.links.find_one({"plan": liked_planid}, {"_id": 1})
+            print("Liked plan Shortlink:", liked_plan_shortlink)
+            likes.append(liked_plan_shortlink["_id"])
+        user_result["likesGiven"] = likes
 
-            user_data["plansCreated"] = []
+        print("plans as list: ", plans_created)
+        user_result["plansCreated"] = plans_created
 
-            print("User Creation result", user_creation_result)
-            return user_data
+        return user_result
+    else:
+        print("User profile not found, creating profile.")
+        user_creation_result = db.users.insert_one(
+            user_data := {
+                "_id": sub,
+                "displayName": "",
+                "public": False,
+                "profileViews": 0,
+                "likesGiven": [],
+                "profilePicture": None,
+                "bio": "",
+            }
+        )
+
+        user_data["plansCreated"] = []
+
+        print("User Creation result", user_creation_result)
+        return user_data
 
 
 @router.patch("", include_in_schema=False)
